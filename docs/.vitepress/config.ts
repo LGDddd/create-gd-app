ef5a6ffdae2e746f58a3d12c77136743bfa50578
liguodi <guodi_li@fpi-inc.com>
2023-09-01 14:18:09 +0800

    feat: 集成文档工具

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    base: '/docs-create-app/',
+    title: 'create-gd-app',
+    description: '创建项目工具CLI文档',
+    themeConfig: {
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/LGDddd/create-gd-app' },
+        ],
+    },
+})
